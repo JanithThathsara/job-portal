0ba4c248dba46708569a18d6b95fca8a662d5af7
Jnaith Thathsara <thathsaralk@gmail.com>
2025-11-17 23:58:38 +0530

    update NAv 1

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations } from '../assets/assets'
 import JobCard from './JobCard'
 
 const JobListing = () => {
 
-    const {isSearched, searchFilter, setSearchFilter, jobs} = useContext(AppContext)   
+    const {isSearched, searchFilter, setSearchFilter, jobs} = useContext(AppContext)
+    
+    const [showFilter,setShowFilter] = useState(false)
+
+    const [currentPage, setCurrentPage] = useState(1)
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
 
@@ -34,8 +38,14 @@ const JobListing = () => {
                 </>
             )
         }
+
+        <button onClick ={e => setShowFilter(prev => !prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+            {showFilter ? "close" : "Filters"}
+        </button>
+
+
         {/* category Filter */}
-        <div className='max-lg:hidden '>
+        <div className= {showFilter ? "" : "max-lg:hidden"}>
             <h4 className='font-medium text-lg py-4'>Search by category</h4>
             <ul className='space-y-4 text-gray-600'> 
                 {
@@ -51,7 +61,7 @@ const JobListing = () => {
         </div>
 
         {/* location filter */}
-         <div className='max-lg:hidden '>
+         <div className= {showFilter ? "" : "max-lg:hidden"}>
             <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
             <ul className='space-y-4 text-gray-600'> 
                 {
@@ -78,6 +88,17 @@ const JobListing = () => {
             ))}
 
         </div>
+
+        {/* pagination */}
+
+        {jobs.length > 0 && (
+           <div>
+            <a href="">
+                <img src={assets.left_arrow_icon} alt="" />
+            </a>
+            {Array.from({length:Math.ceil(jobs.length/6)}).map(_.index)=>()}
+           </div> 
+        )} 
     </section>
     </div>
   )
