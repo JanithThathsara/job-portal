76f8a2a64343ca64eb3712d35c3e6dd4f5f80539
Jnaith Thathsara <thathsaralk@gmail.com>
2025-11-20 23:51:36 +0530

    create Application and update and improve its

---
+++ client/src/components/RecruiterLogin.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+
+const RecruiterLogin = () => {
+
+    const [state, setState] =useState('Login')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    return (
+        <div>
+            <form action="">
+                <h1>Recruiter {state}</h1>
+                <p>Welcome back! please sign in to continue</p>
+                <>
+                <div>
+                    <img src={assets.person_icon} alt="" />
+                    <input onChange={e => setName(e.target.value)} value={name} type="text" placeholder='Company name' required />
+                </div>
+
+                <div>
+                    <img src={assets.email_icon} alt="" />
+                    <input onChange={e => setEmail(e.target.value)} value={email} type="email" placeholder='Email Id' required />
+                </div>
+
+                <div>
+                    <img src={assets.lock_icon} alt="" />
+                    <input onChange={e => setPassword(e.target.value)} value={password} type="password" placeholder='Password' required />
+                </div>
+                </>
+
+                <button>
+                    {state === 'Login' ? 'login' : 'create account'}
+                </button>
+            </form>
+        </div>
+    )
+
+    const [image,setImage] = useState(false)
+
+    const [isTextDataSubmited, setIsTextDataSubmited] = useState(false)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default RecruiterLogin
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,8 @@ export const AppContextProvider = (props) => {
 
     const [jobs, setJobs] = useState([])
 
+    const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
+
     // Function to fetch jobs
 
     const fetchJobs = async ()=> {
@@ -28,6 +30,7 @@ export const AppContextProvider = (props) => {
         setSearchFilter,searchFilter,
         isSearched,setIsSearched,
         jobs,setJobs,
+        showRecruiterLogin,setShowRecruiterLogin,
     }
 
     return (
