a49e3f6f44040b6f28bc81b20e2dfa6761b1e611
Jnaith Thathsara <thathsaralk@gmail.com>
2025-11-30 15:24:38 +0530

    create add-job 1

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Quill from 'quill'
+import { JobCategories, JobLocations } from '../assets/assets'
 
 const AddJob = () => {
+
+  const [title, setTitle] = useState('')
+  const [location, setLocation] = useState('Bangalore')
+  const [category, setCategory] = useState('programming')
+  const [level, setLevel] = useState('Beginner level')
+  const [salary, setSalary] = useState(0)
+
+  const editorRef = useRef(null)
+  const quillRef = useRef(null)
+
+  useEffect(() => {
+    //initial Quill only once
+    if(!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current,{
+        theme:'snow',
+      })
+    }
+  }, [])
+
   return (
-    <div>AddJob</div>
+    <form>
+      <div>
+        <p>Job title</p>
+        <input type="text" placeholder='Type here' onChange={e => setTitle(e.target.value)} value={title} required/>
+      </div>
+      
+      <div>
+        <p>Job Description</p>
+        <div ref={editorRef}>
+
+        </div>
+      </div>
+
+      <div>
+
+        <div>
+          <p>Job category</p>
+          <select onChange={e => setCategory(e.target.value)}>
+            {JobCategories.map((category, index)=> (
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <p>Job location</p>
+          <select onChange={e => setLocation(e.target.value)}>
+            {JobLocations.map((location, index)=> (
+              <option key={index} value={location}>{location}</option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <p>Job Level</p>
+          <select onChange={e => setLevel(e.target.value)}>
+            <option value="Beginner level">Beginner level</option>
+            <option value="Intermediate level">Intermediate level</option>
+            <option value="Senior level">Senior level</option>
+          </select>
+        </div>
+
+      </div>
+    </form>
   )
 }
 
